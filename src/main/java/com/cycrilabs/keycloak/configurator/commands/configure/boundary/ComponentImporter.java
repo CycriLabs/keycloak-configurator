700f8cef5e3d4a8dc6a4411d02419a7ebd8a8c0e
Marc Scheib <marc.scheib@gmx.de>
2024-04-14 10:44:48 +0200

    feat: add importer for components (#48)

---
+++ src/main/java/com/cycrilabs/keycloak/configurator/commands/configure/boundary/ComponentImporter.java
@@ -0,0 +1,74 @@
+package com.cycrilabs.keycloak.configurator.commands.configure.boundary;
+
+import java.nio.file.Path;
+import java.util.Optional;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.ws.rs.ClientErrorException;
+import jakarta.ws.rs.core.Response;
+
+import org.keycloak.representations.idm.ComponentRepresentation;
+
+import com.cycrilabs.keycloak.configurator.shared.control.JsonUtil;
+import com.cycrilabs.keycloak.configurator.shared.entity.EntityType;
+
+import io.quarkus.logging.Log;
+
+@ApplicationScoped
+public class ComponentImporter extends AbstractImporter {
+    @Override
+    public EntityType getType() {
+        return EntityType.COMPONENT;
+    }
+
+    @Override
+    protected Object importFile(final Path file) {
+        final ComponentRepresentation component =
+                JsonUtil.loadEntity(file, ComponentRepresentation.class);
+
+        final String[] fileNameParts = file.toString().split(PATH_SEPARATOR);
+        final String realmName = fileNameParts[fileNameParts.length - 3];
+
+        if (component.getParentId() != null) {
+            final ComponentRepresentation parent =
+                    findComponentByName(realmName, component.getParentId());
+            if (parent == null) {
+                Log.errorf(
+                        "Could not import component from file '%s' for realm '%s' because of missing parent '%s'.",
+                        file, realmName, component.getParentId());
+                return null;
+            }
+
+            // adapt the parent id from a string to the real id of the parent
+            component.setParentId(parent.getId());
+        }
+
+        try (final Response response = keycloak.realm(realmName)
+                .components()
+                .add(component)) {
+            if (response.getStatus() == 409) {
+                Log.errorf("Could not import component from file for realm '%s': %s", realmName,
+                        extractError(response).getErrorMessage());
+            } else {
+                Log.infof("Component '%s' imported for realm '%s'.", component.getName(),
+                        realmName);
+            }
+        } catch (final ClientErrorException e) {
+            Log.errorf("Could not import component from file for realm '%s': %s", realmName,
+                    e.getMessage());
+            return null;
+        }
+
+        return findComponentByName(realmName, component.getName());
+    }
+
+    private ComponentRepresentation findComponentByName(final String realmName, final String name) {
+        final Optional<ComponentRepresentation> importedComponent = keycloak.realm(realmName)
+                .components()
+                .query(realmName)
+                .stream()
+                .filter(c -> c.getName().equals(name))
+                .findFirst();
+        return importedComponent.orElse(null);
+    }
+}
