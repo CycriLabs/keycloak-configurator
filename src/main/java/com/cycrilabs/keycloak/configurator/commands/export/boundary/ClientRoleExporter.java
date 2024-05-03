ad15e05d8491637b255b09f79fb14e80802fac80
Marc Scheib <marc.scheib@tech11.com>
2024-05-03 16:50:24 +0200

    fix: export client roles of each client if no specific requested (#58)

--- src/main/java/com/cycrilabs/keycloak/configurator/commands/export/boundary/ClientRoleExporter.java
+++ src/main/java/com/cycrilabs/keycloak/configurator/commands/export/boundary/ClientRoleExporter.java
@@ -2,6 +2,7 @@ package com.cycrilabs.keycloak.configurator.commands.export.boundary;
 
 import jakarta.enterprise.context.ApplicationScoped;
 
+import org.apache.commons.lang3.StringUtils;
 import org.keycloak.representations.idm.ClientRepresentation;
 import org.keycloak.representations.idm.RoleRepresentation;
 
@@ -19,43 +20,61 @@ public class ClientRoleExporter extends AbstractExporter {
 
     @Override
     protected void exportEntity(final String entityName) {
-        final ClientRepresentation clientRepresentation =
-                keycloak.realm(configuration.getRealmName())
-                        .clients()
-                        .findByClientId(configuration.getClient())
-                        .stream()
-                        .findFirst()
-                        .orElseThrow(() -> new RuntimeException("Client not found."));
+        if (StringUtils.isNotBlank(configuration.getClient())) {
+            keycloak.realm(configuration.getRealmName())
+                    .clients()
+                    .findByClientId(configuration.getClient())
+                    .stream()
+                    .findFirst()
+                    .ifPresent(client -> exportEntity(entityName, client));
+        } else {
+            keycloak.realm(configuration.getRealmName())
+                    .clients()
+                    .findAll()
+                    .forEach(client -> exportEntity(entityName, client));
+        }
+    }
+
+    private void exportEntity(final String entityName, final ClientRepresentation client) {
         final RoleRepresentation entity = keycloak.realm(configuration.getRealmName())
                 .clients()
-                .get(clientRepresentation.getId())
+                .get(client.getId())
                 .roles()
                 .get(entityName)
                 .toRepresentation();
-
         Log.infof("Exporting client role '%s'.", entity.getName());
-        writeFile(JsonUtil.toJson(entity), entity.getName(),
-                configuration.getRealmName(), clientRepresentation.getClientId());
+        writeFile(JsonUtil.toJson(entity), entity.getName(), configuration.getRealmName(),
+                client.getClientId());
     }
 
     @Override
     protected void exportEntities() {
-        final ClientRepresentation clientRepresentation =
-                keycloak.realm(configuration.getRealmName())
-                        .clients()
-                        .findByClientId(configuration.getClient())
-                        .stream()
-                        .findFirst()
-                        .orElseThrow(() -> new RuntimeException("Client not found."));
+        if (StringUtils.isNotBlank(configuration.getClient())) {
+            keycloak.realm(configuration.getRealmName())
+                    .clients()
+                    .findByClientId(configuration.getClient())
+                    .stream()
+                    .findFirst()
+                    .ifPresent(this::exportEntities);
+        } else {
+            keycloak.realm(configuration.getRealmName())
+                    .clients()
+                    .findAll()
+                    .forEach(this::exportEntities);
+        }
+    }
+
+    private void exportEntities(final ClientRepresentation client) {
         keycloak.realm(configuration.getRealmName())
                 .clients()
-                .get(clientRepresentation.getId())
+                .get(client.getId())
                 .roles()
                 .list()
                 .forEach(entity -> {
-                    Log.infof("Exporting client role '%s'.", entity.getName());
+                    Log.infof("Exporting client role '%s' for client '%s'.", entity.getName(),
+                            client.getClientId());
                     writeFile(JsonUtil.toJson(entity), entity.getName(),
-                            configuration.getRealmName(), clientRepresentation.getClientId());
+                            configuration.getRealmName(), client.getClientId());
                 });
     }
 }
