ddd4ac7138b1f936e8ec71096e51785c10333a03
Marc Scheib <marc.scheib@tech11.com>
2024-09-22 17:33:32 +0200

    fix: include composites of realm role export (#62)

--- src/main/java/com/cycrilabs/keycloak/configurator/commands/export/boundary/RealmRoleExporter.java
+++ src/main/java/com/cycrilabs/keycloak/configurator/commands/export/boundary/RealmRoleExporter.java
@@ -1,9 +1,17 @@
 package com.cycrilabs.keycloak.configurator.commands.export.boundary;
 
-import jakarta.enterprise.context.ApplicationScoped;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+
+import org.keycloak.representations.idm.ClientRepresentation;
 import org.keycloak.representations.idm.RoleRepresentation;
 
+import com.cycrilabs.keycloak.configurator.commands.configure.control.EntityStore;
 import com.cycrilabs.keycloak.configurator.shared.control.JsonUtil;
 import com.cycrilabs.keycloak.configurator.shared.entity.EntityType;
 
@@ -11,6 +19,13 @@ import io.quarkus.logging.Log;
 
 @ApplicationScoped
 public class RealmRoleExporter extends AbstractExporter {
+    EntityStore entityStore;
+
+    @Inject
+    public RealmRoleExporter(final EntityStore entityStore) {
+        this.entityStore = entityStore;
+    }
+
     @Override
     public EntityType getType() {
         return EntityType.REALM_ROLE;
@@ -31,10 +46,46 @@ public class RealmRoleExporter extends AbstractExporter {
         keycloak.realm(configuration.getRealmName())
                 .roles()
                 .list()
+                .stream()
+                .map(this::createRealmRoleWithComposites)
                 .forEach(entity -> {
                     Log.infof("Exporting realm role '%s'.", entity.getName());
                     writeFile(JsonUtil.toJson(entity), entity.getName(),
                             configuration.getRealmName());
                 });
+    }
+
+    private RoleRepresentation createRealmRoleWithComposites(final RoleRepresentation entity) {
+        final Set<RoleRepresentation> compositeRoles = keycloak.realm(configuration.getRealmName())
+                .rolesById()
+                .getRoleComposites(entity.getId());
+
+        final RoleRepresentation.Composites composites = new RoleRepresentation.Composites();
+        composites.setRealm(new HashSet<>());
+        composites.setClient(new HashMap<>());
+
+        for (final RoleRepresentation compositeRole : compositeRoles) {
+            Log.info(compositeRole.getDescription());
+            final ClientRepresentation client = loadClient(compositeRole.getContainerId());
+            composites.getClient().computeIfAbsent(client.getClientId(), k -> new ArrayList<>())
+                    .add(compositeRole.getName());
+        }
+
+        entity.setComposites(composites);
+        return entity;
+    }
+
+    private ClientRepresentation loadClient(final String id) {
+        final ClientRepresentation client = entityStore.getClient(configuration.getRealmName(), id);
+        if (client != null) {
+            return client;
+        }
+
+        final ClientRepresentation loadedClient = keycloak.realm(configuration.getRealmName())
+                .clients()
+                .get(id)
+                .toRepresentation();
+        entityStore.addClient(configuration.getRealmName(), loadedClient);
+        return loadedClient;
     }
 }
