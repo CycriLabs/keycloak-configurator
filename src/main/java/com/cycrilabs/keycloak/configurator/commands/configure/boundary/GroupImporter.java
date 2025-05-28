1a798717eea77d58c21d8cb551efa91f379fb0d6
Marc Scheib <marc.scheib@tech11.com>
2025-05-28 21:52:19 +0200

    feat(configure): add realm role import support to group importer (#90)

--- src/main/java/com/cycrilabs/keycloak/configurator/commands/configure/boundary/GroupImporter.java
+++ src/main/java/com/cycrilabs/keycloak/configurator/commands/configure/boundary/GroupImporter.java
@@ -1,5 +1,6 @@
 package com.cycrilabs.keycloak.configurator.commands.configure.boundary;
 
+import java.util.List;
 import java.util.Optional;
 
 import jakarta.enterprise.context.ApplicationScoped;
@@ -7,6 +8,7 @@ import jakarta.ws.rs.ClientErrorException;
 import jakarta.ws.rs.core.Response;
 
 import org.keycloak.representations.idm.GroupRepresentation;
+import org.keycloak.representations.idm.RoleRepresentation;
 
 import com.cycrilabs.keycloak.configurator.commands.configure.entity.ConfigurationFile;
 import com.cycrilabs.keycloak.configurator.commands.configure.entity.ImporterStatus;
@@ -58,17 +60,39 @@ public class GroupImporter extends AbstractImporter<GroupRepresentation> {
         Log.infof("Loaded imported group '%s' from realm '%s'.", importedGroup.getName(),
                 realmName);
 
+        // add realm roles to group: they are not added automatically when a new group is created
+        addRealmRoles(realmName, importedGroup, group.getRealmRoles());
+
         applyGroupHierarchy(realmName, group.getPath(), importedGroup);
 
         return importedGroup;
+    }
+
+    private void addRealmRoles(final String realmName, final GroupRepresentation importedGroup,
+            final List<String> realmRoles) {
+        if (realmRoles == null || realmRoles.isEmpty()) {
+            return;
+        }
+
+        Log.infof("Adding realm roles to group '%s' in realm '%s'.", importedGroup.getName(),
+                realmName);
+        final List<RoleRepresentation> roleRepresentations = realmRoles
+                .stream()
+                .map(roleName -> keycloakCache.getRoleByName(realmName, roleName))
+                .toList();
+        keycloak.realm(realmName)
+                .groups()
+                .group(importedGroup.getId())
+                .roles()
+                .realmLevel()
+                .add(roleRepresentations);
     }
 
     /**
      * This implements a naive approach of creating the group hierarchy for the given set of group
      * configurations.
      * It assumes that the groups are imported in the correct order in regard to their hierarchy and
-     * the hierarchy
-     * is linear as well.
+     * the hierarchy is linear as well.
      */
     private void applyGroupHierarchy(final String realmName, final String path,
             final GroupRepresentation group) {
@@ -77,23 +101,25 @@ public class GroupImporter extends AbstractImporter<GroupRepresentation> {
         }
 
         final String[] groupHierarchy = path.split("/");
-        if (groupHierarchy.length > 2) {
-            final String groupName = groupHierarchy[groupHierarchy.length - 2];
-            final Optional<GroupRepresentation> potentialGroup = keycloak.realm(realmName)
+        if (groupHierarchy.length <= 2) {
+            return;
+        }
+
+        final String groupName = groupHierarchy[groupHierarchy.length - 2];
+        final Optional<GroupRepresentation> potentialGroup = keycloak.realm(realmName)
+                .groups()
+                .query(groupName, false, null, null, false)
+                .stream()
+                .filter(grp -> grp.getName().equals(groupName))
+                .findFirst();
+        if (potentialGroup.isPresent()) {
+            final GroupRepresentation parentGroup = potentialGroup.get();
+            try (final Response response = keycloak.realm(realmName)
                     .groups()
-                    .query(groupName, false, null, null, false)
-                    .stream()
-                    .filter(grp -> grp.getName().equals(groupName))
-                    .findFirst();
-            if (potentialGroup.isPresent()) {
-                final GroupRepresentation parentGroup = potentialGroup.get();
-                try (final Response response = keycloak.realm(realmName)
-                        .groups()
-                        .group(parentGroup.getId())
-                        .subGroup(group)) {
-                    Log.infof("Adding group '%s' as child to '%s'.", group.getName(),
-                            parentGroup.getName());
-                }
+                    .group(parentGroup.getId())
+                    .subGroup(group)) {
+                Log.infof("Adding group '%s' as child to '%s'.", group.getName(),
+                        parentGroup.getName());
             }
         }
     }
