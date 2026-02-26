eba4edc4ec4163841b27be2108ea15c9ef930f33
Daniel Stegmeier <3967403+dasteg@users.noreply.github.com>
2026-02-26 10:39:03 +0100

    added cli option to take password using an environment variable

--- src/main/java/com/cycrilabs/keycloak/configurator/shared/control/KeycloakOptions.java
+++ src/main/java/com/cycrilabs/keycloak/configurator/shared/control/KeycloakOptions.java
@@ -3,20 +3,28 @@ package com.cycrilabs.keycloak.configurator.shared.control;
 import picocli.CommandLine;
 
 public class KeycloakOptions {
-    @CommandLine.Option(required = true, names = { "-s", "--server" },
-            description = "Keycloak server that will be configured.",
-            scope = CommandLine.ScopeType.INHERIT)
-    String server = "";
-    @CommandLine.Option(required = true, names = { "-u", "--username" },
-            description = "Username of the admin user that is used for configuration.",
-            scope = CommandLine.ScopeType.INHERIT)
-    String username = "";
-    @CommandLine.Option(required = true, names = { "-p", "--password" },
-            description = "Password of the admin user that is used for configuration.",
-            scope = CommandLine.ScopeType.INHERIT, arity = "0..1", interactive = true)
-    String password = "";
-    @CommandLine.Option(names = { "--dry-run" },
-            description = "If set, the configuration will not be applied to the server.",
-            scope = CommandLine.ScopeType.INHERIT)
-    boolean dryRun = false;
+	@CommandLine.Option(required = true, names = { "-s", "--server" }, 
+			description = "Keycloak server that will be configured.", 
+			scope = CommandLine.ScopeType.INHERIT)
+	String server = "";
+
+	@CommandLine.Option(required = true, names = { "-u", "--username" }, 
+			description = "Username of the admin user that is used for configuration.", 
+			scope = CommandLine.ScopeType.INHERIT)
+	String username = "";
+
+	@CommandLine.Option(names = { "-p", "--password" }, 
+			description = "Password of the admin user that is used for configuration.", 
+			scope = CommandLine.ScopeType.INHERIT, arity = "0..1", interactive = true)
+	String password = "";
+
+	@CommandLine.Option(names = { "-p:env", "--password:env" }, 
+			description = "Password of the admin user that is used for configuration taken from an environment variable.", 
+			scope = CommandLine.ScopeType.INHERIT, arity = "0..1", interactive = true)
+	String passwordEnv = "";
+
+	@CommandLine.Option(names = { "--dry-run" }, 
+			description = "If set, the configuration will not be applied to the server.", 
+			scope = CommandLine.ScopeType.INHERIT)
+	boolean dryRun = false;
 }
--- src/main/java/com/cycrilabs/keycloak/configurator/shared/entity/KeycloakConfiguration.java
+++ src/main/java/com/cycrilabs/keycloak/configurator/shared/entity/KeycloakConfiguration.java
@@ -1,29 +1,39 @@
 package com.cycrilabs.keycloak.configurator.shared.entity;
 
+import com.cycrilabs.keycloak.configurator.shared.control.StringUtil;
+
 import lombok.Getter;
 
+import picocli.CommandLine;
 import picocli.CommandLine.ParseResult;
 
 @Getter
 public class KeycloakConfiguration {
-    private String server;
-    private String username;
-    private String password;
-    private boolean dryRun;
+	private String server;
+	private String username;
+	private String password;
+	private boolean dryRun;
 
-    public KeycloakConfiguration() {
-        // required to avoid "No default constructor for class" error
-    }
+	public KeycloakConfiguration() {
+		// required to avoid "No default constructor for class" error
+	}
 
-    public KeycloakConfiguration(final ParseResult parseResult) {
-        server = getMatchedOption(parseResult, "-s");
-        username = getMatchedOption(parseResult, "-u");
-        password = getMatchedOption(parseResult, "-p");
-        dryRun = this.<Boolean>getMatchedOption(parseResult, "--dry-run").booleanValue();
-    }
+	public KeycloakConfiguration(final ParseResult parseResult) {
+		server = getMatchedOption(parseResult, "-s");
+		username = getMatchedOption(parseResult, "-u");
+		password = getMatchedOption(parseResult, "--password");
+		if (StringUtil.isBlank(password)) {
+			String passwordEnv = getMatchedOption(parseResult, "--password:env");
+			password = System.getenv(passwordEnv);
+		}
+		if (StringUtil.isBlank(password)) {
+			CommandLine.Model.CommandSpec spec = parseResult.subcommand().commandSpec();
+			throw new CommandLine.ParameterException(spec.commandLine(), "Password required");
+		}
+		dryRun = this.<Boolean>getMatchedOption(parseResult, "--dry-run").booleanValue();
+	}
 
-    protected <T> T getMatchedOption(final ParseResult parseResult, final String name) {
-        return parseResult.subcommand().commandSpec().optionsMap().get(name).getValue();
-    }
+	protected <T> T getMatchedOption(final ParseResult parseResult, final String name) {
+		return parseResult.subcommand().commandSpec().optionsMap().get(name).getValue();
+	}
 }
-
